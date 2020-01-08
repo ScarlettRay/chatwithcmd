c30cb8e943337075899c157b7caa307a6a47cc63
ScarlettRay <923908153@qq.com>
2020-01-08 16:24:55 +0800

    :beer: 提交一波代码

--- src/main/java/core/channel/DealMesSendChannelHandler.java
+++ src/main/java/core/channel/DealMesSendChannelHandler.java
@@ -4,16 +4,18 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.SimpleChannelInboundHandler;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author Administrator
  * @create 2020-01-02 17:29:37
  * <p>处理服务端返回的数据
  */
+@Slf4j
 public class DealMesSendChannelHandler  extends SimpleChannelInboundHandler<ByteBuf>{
 
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
-
+        log.info("消息发出");
     }
 }
--- src/main/java/net/MyChatClient.java
+++ src/main/java/net/MyChatClient.java
@@ -4,30 +4,40 @@ import com.alibaba.fastjson.JSON;
 import common.Server;
 import core.channel.DealMesSendChannelHandler;
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.DelimiterBasedFrameDecoder;
 import io.netty.handler.codec.Delimiters;
 import io.netty.handler.codec.http.HttpServerCodec;
+import lombok.extern.slf4j.Slf4j;
 import util.Message;
 import util.Result;
+
+import java.util.List;
 
 /**
  * @author Administrator
  * @create 2019-12-25 17:15:25
  * <p>聊天客户端，发送请求
  */
+@Slf4j
 public class MyChatClient {
 
-    public Result sendMessage(Message message, Server server){
+    private Bootstrap client = null;
+    private EventLoopGroup group = null;
+
+    public static final MyChatClient MYCHATCLIENT = new MyChatClient();
+    /**
+     * 初始化客户端
+     */
+    public void initlize(){
         // 首先，netty通过ServerBootstrap启动服务端
-        Bootstrap client = new Bootstrap();
+        client = new Bootstrap();
 
         //第1步 定义线程组，处理读写和链接事件，没有了accept事件
-        EventLoopGroup group = new NioEventLoopGroup();
+        group = new NioEventLoopGroup();
         client.group(group );
 
         //第2步 绑定客户端通道
@@ -36,7 +46,7 @@ public class MyChatClient {
         //第3步 给NIoSocketChannel初始化handler， 处理读写事件
         client.handler(new ChannelInitializer<NioSocketChannel>() {  //通道是NioSocketChannel
             @Override
-            protected void initChannel(NioSocketChannel ch) throws Exception {
+            protected void initChannel(NioSocketChannel ch){
                 //字符串编码器，一定要加在SimpleClientHandler 的上面
                 ch.pipeline().addLast("http-codec", new HttpServerCodec());
                 ch.pipeline().addLast(new DelimiterBasedFrameDecoder(
@@ -45,21 +55,38 @@ public class MyChatClient {
                 ch.pipeline().addLast(new DealMesSendChannelHandler());
             }
         });
-
-        //连接服务器
-        ChannelFuture future = null;
-        try {
-            future = client.connect(server.getIp(), server.getPort()).sync();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        future.channel().writeAndFlush(JSON.toJSON(message));
-
-        //当通道关闭了，就继续往下走
-        try {
-            future.channel().closeFuture().sync();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
+
+    public Result sendMessage(Message message, Server server){
+        //连接服务器
+        try {
+            ChannelFuture future = client.connect(server.getIp(), server.getPort()).sync();
+            future.channel().writeAndFlush(JSON.toJSON(message));
+            //当通道关闭了，就继续往下走
+            future.channel().closeFuture().sync();
+            return Result.OK;
+        } catch (InterruptedException e) {
+            log.error("消息发送出现异常！"+ e.getMessage());
+        }finally {
+            group.shutdownGracefully();
+        }
+        return Result.ERROR;
+    }
+
+    /**
+     * 批量发送
+     * @param message
+     * @param servers
+     * @return
+     */
+    public Result batchSendMessage(Message message,List<Server> servers){
+        for (Server e : servers) {
+            Result result = sendMessage(message, e);
+            if(result.isOK()){
+                //TODO 成功或失败怎么处理
+            }
+        }
+        return Result.OK;
+    }
+
 }
